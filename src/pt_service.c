68435a28f62ff8089f6d9fbad24610f369768e53
Ji-hoon Jung <jh8801.jung@samsung.com>
2014-11-25 20:01:42 +0900

    change the license from Flora to Apache 2.0
    Signed-off-by: Ji-hoon Jung <jh8801.jung@samsung.com>
    Change-Id: I28bc08ce214f90d0391a33d7e5eae1468d79922e

--- src/pt_service.c
+++ src/pt_service.c
@@ -1,14 +1,14 @@
 /*
 *  Printservice
 *
-* Copyright 2012  Samsung Electronics Co., Ltd
-
-* Licensed under the Flora License, Version 1.1 (the "License");
+* Copyright (c) 2012-2013 Samsung Electronics Co., Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
-
-* http://floralicense.org/license/
-
+*
+*					http://www.apache.org/licenses/LICENSE-2.0
+*
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
